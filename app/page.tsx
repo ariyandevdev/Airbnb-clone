74a81fb7b7a69c91240446012bd371c48a67576b
Ariyan Yarahmadi <yarahmadiariyan@gmail.com>
2025-09-15 14:42:07 +0330

    install shadcn ui and add the components ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Button variant="outline" size="lg" className=" capitalize m-8">
+        Click me
+      </Button>
+      <Button
+        variant="outline"
+        size="lg"
+        className="bg-blue-500 capitalize m-8"
+      >
+        Click me
+      </Button>
+    </div>
+  );
 };
 
 export default HomePage;
